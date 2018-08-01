2166a5a90d0166048eb8188c6342bd4e6ad61508
jg <992975556@qq.com>
2018-08-01 15:06:14 +0800

    Update resultbody
    +getErrors
    +addError
    +addErrors
    +addDao
    +getDao

--- core/src/main/java/com/group1/core/utils/ResultBody.java
+++ core/src/main/java/com/group1/core/utils/ResultBody.java
@@ -1,16 +1,29 @@
 package com.group1.core.utils;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.validation.ObjectError;
+
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ResultBody implements Serializable {
+
+    public static final String STATUS_ERROR = "0";
+    public static final String STATUS_SUCCESS = "1";
+
     private String status;
     private String message;
-    private Map<String,Object> data = new LinkedHashMap<>();
+    private Map<String,Object> data;
 
     public String getStatus() {
+        if (status == null) {
+            List errors = null;
+            if (data != null && (errors = (List) data.get("errors")) != null && errors.size() > 0) {
+                return STATUS_ERROR;
+            }else{
+                return STATUS_SUCCESS;
+            }
+        }
         return status;
     }
 
@@ -26,9 +39,6 @@ public class ResultBody implements Serializable {
         this.message = message;
     }
 
-    public Object getData(String key) {
-        return data.get(key);
-    }
 
     public void setData(String key,Object value) {
         data.put(key,value);
@@ -40,5 +50,42 @@ public class ResultBody implements Serializable {
 
     public void setData(Map<String, Object> data) {
         this.data = data;
+    }
+
+    public void addError(String key, String val) {
+        ObjectError error = new ObjectError(key, val);
+        getErrors().add(error);
+    }
+
+    @JsonIgnore
+    public List<ObjectError> getErrors() {
+        List<ObjectError> errors = (List<ObjectError>) getData("errors");
+        if (errors == null) {
+            errors = new ArrayList<>();
+            addData("errors", errors);
+        }
+        return errors;
+    }
+
+    public void addError(ObjectError error) {
+        getErrors().add(error);
+    }
+
+    public void addErrors(List<ObjectError> errors){
+        getErrors().addAll(errors);
+    }
+
+    public void addData(String key, Object value) {
+        if (data == null) {
+            data = new LinkedHashMap<>();
+        }
+        data.put(key, value);
+    }
+
+    public Object getData(String key){
+        if (data == null) {
+            data = new LinkedHashMap<>();
+        }
+        return data.get(key);
     }
 }
