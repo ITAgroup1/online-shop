11b39d4e119c3320e28ab906ef07bff2ac0d442a
松松 <25002269512@qq.com>
2018-08-02 13:14:44 +0800

    1、fix core entity bug

--- merchant/src/main/java/com/group1/merchant/dao/RecipeRepository.java
+++ merchant/src/main/java/com/group1/merchant/dao/RecipeRepository.java
@@ -1,6 +1,6 @@
 package com.group1.merchant.dao;
 
-import com.group1.core.entity.cmdb.recipe.Recipe;
+import com.group1.core.entity.recipe.Recipe;
 import com.group1.core.utils.base.JpaRepository;
 
 public interface RecipeRepository extends JpaRepository<Recipe,String> {
--- merchant/src/main/java/com/group1/merchant/service/impl/ShopServiceImpl.java
+++ merchant/src/main/java/com/group1/merchant/service/impl/ShopServiceImpl.java
@@ -1,10 +1,22 @@
 package com.group1.merchant.service.impl;
 
+import com.group1.core.entity.shop.Shop;
+import com.group1.merchant.dao.ShopRepository;
 import com.group1.merchant.service.ShopService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
+
 @Service("shopService")
-@Transactional
 public class ShopServiceImpl implements ShopService {
+
+    @Resource
+    private ShopRepository shopRepository;
+
+    @Override
+    @Transactional
+    public Shop add(Shop shop) {
+        return shopRepository.save(shop);
+    }
 }
