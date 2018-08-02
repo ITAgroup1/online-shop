c79647b7b2e3968305d22c04913904af585cc350
jingjing1996 <1372031421@qq.com>
2018-08-02 22:36:24 +0800

    Merge branch 'dev' of https://github.com/ITAgroup1/online-shop into joy20180801
    # Conflicts:
    #	core/src/main/java/com/group1/core/entity/client/Client.java

--- client/src/main/java/com/group1/client/service/impl/ClientServiceImpl.java
+++ client/src/main/java/com/group1/client/service/impl/ClientServiceImpl.java
@@ -3,9 +3,14 @@ package com.group1.client.service.impl;
 import com.group1.client.dao.ClientRepository;
 import com.group1.client.service.ClientService;
 import com.group1.core.entity.client.Client;
+import com.group1.core.entity.complaint.Complaint;
+import com.group1.core.utils.JsonUtil;
+import com.group1.core.utils.ResultBody;
+import com.group1.core.utils.jms.ProducerService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import javax.jms.Destination;
 
 @Service("clientService")
 
@@ -13,6 +18,12 @@ public class ClientServiceImpl implements ClientService {
 
     @Resource
     private ClientRepository clientRepository;
+
+    @Resource(name = "queueDestination")
+    private Destination destination;
+
+    @Resource(name = "producerService")
+    private ProducerService producerService;
 
 
     @Override
@@ -28,5 +39,17 @@ public class ClientServiceImpl implements ClientService {
     @Override
     public Client login(Client client) {
         return clientRepository.login(client);
+    }
+
+    @Override
+    public ResultBody complain(Complaint complaint) {
+        ResultBody resultBody = new ResultBody();
+        resultBody.addData("type","Complaint");
+        resultBody.addData("complaint",complaint);
+        String complaintJsonString = JsonUtil.objectToJson(resultBody);
+        producerService.sendMessage(destination,complaintJsonString);
+        return resultBody;
+
+
     }
 }
