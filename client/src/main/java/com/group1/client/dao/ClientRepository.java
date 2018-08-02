081e55147206fe8e03814aa81155203624bce72b
Song <2500269512@qq.com>
2018-08-02 09:16:49 +0800

    Delete ClientRepository.java

--- client/src/main/java/com/group1/client/dao/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.group1.client.dao;
-
-import com.group1.core.entity.client.Client;
-import com.group1.core.utils.base.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client,String> {
-    Client login(String loginName,String password);
-    Client update(Client client,String id);
-}
