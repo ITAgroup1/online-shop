c166ef7ea1ece9dceb8b30dacac347f210c2a302
jg <992975556@qq.com>
2018-08-05 15:23:10 +0800

    Update order

--- merchant/src/main/java/com/group1/merchant/controller/ComplaintController.java
+++ merchant/src/main/java/com/group1/merchant/controller/ComplaintController.java
@@ -6,18 +6,19 @@ import com.group1.merchant.service.ComplaintService;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/complaint")
-@SessionAttributes("merchant")
 public class ComplaintController {
 
     @Resource(name = "complaintService")
     private ComplaintService complaintService;
 
     @GetMapping
-    public ResultBody getComplaints(@ModelAttribute Merchant merchant) {
-        if(merchant != null) {
+    public ResultBody getComplaints(HttpSession session) {
+        Merchant merchant = (Merchant) session.getAttribute("merchant");
+        if(merchant == null) {
             return null;
         }
         ResultBody resultBody = complaintService.getComplaints(merchant.getId());
